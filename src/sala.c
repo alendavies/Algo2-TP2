e30b2df2ace9e0641706820ab3650c79439de3a0
alendavieslec <alendavies@gmail.com>
2022-06-27 03:34:50 -0300

    jeee

--- src/sala.c
+++ src/sala.c
@@ -7,121 +7,141 @@
 #include <string.h>
 #include "lista.h"
 
-#define MAX_LINEA 1024
-
 typedef struct sala {
 	lista_t *objetos;
 	lista_t *interacciones;
 } sala_t;
 
-sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones)
+#define MAX_CARACTERES 1024
+#define CANT_OBJETOS 20
+#define ERROR -1
+
+lista_t *crear_lista_objetos(FILE *archivo)
 {
+	char linea[MAX_CARACTERES];
+	lista_t *lista_objetos = lista_crear();
+	if(!lista_objetos){
+		return NULL;
+	}
+	
+	while(fgets(linea, MAX_CARACTERES, archivo) != NULL){
+		
+		struct objeto *objeto = objeto_crear_desde_string(linea);
+		if(!objeto){
+			lista_destruir(lista_objetos);
+			return NULL;
+		}
+		lista_objetos = lista_insertar(lista_objetos, objeto);
+		if(!lista_objetos){
+			free(objeto);
+			lista_destruir(lista_objetos);
+			return NULL;
+		}	
+	}
+	return lista_objetos;
+}
+
+lista_t *crear_lista_interacciones(FILE *archivo)
+{
+	char linea[MAX_CARACTERES];
+	lista_t *lista_interacciones = lista_crear();
+	if(!lista_interacciones){
+		return NULL;
+	}
+	
+	while(fgets(linea, MAX_CARACTERES, archivo) != NULL){
+		
+		struct interaccion *interaccion = interaccion_crear_desde_string(linea);
+		if(!interaccion){
+			lista_destruir(lista_interacciones);
+			return NULL;
+		}
+		lista_interacciones = lista_insertar(lista_interacciones, interaccion);
+		if(!lista_interacciones){
+			free(interaccion);
+			lista_destruir(lista_interacciones);
+			return NULL;
+		}	
+	}
+	return lista_interacciones;
+}
+
+sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones)
+{	
 	if(!objetos || !interacciones){
 		return NULL;
 	}
 
-	FILE *archivo_objetos = fopen(objetos, "r");
-	if(!archivo_objetos){
-		return NULL;
+	FILE *arch_objetos = fopen(objetos, "r");
+	if(!arch_objetos){
+        	return NULL;
 	}
 
-	FILE *archivo_interacciones = fopen(interacciones, "r");
-	if (!archivo_interacciones){
-		fclose(archivo_objetos);
-		return NULL;
+	FILE *arch_interacciones = fopen(interacciones, "r");
+	if(!arch_interacciones){
+		fclose(arch_objetos);
+        	return NULL;
 	}
 
-	char linea[MAX_LINEA];
-
-	sala_t *sala = malloc(sizeof(sala_t));
-
+	sala_t *sala = calloc(1, sizeof(struct sala));
 	if(!sala){
-		fclose(archivo_objetos);
-		fclose(archivo_interacciones);
-		free(sala);
+		fclose(arch_objetos);
+		fclose(arch_interacciones);
 		return NULL;
 	}
-
-	lista_t *lista_objetos = malloc(sizeof(lista_t));
-
-	while(fgets(linea, MAX_LINEA, archivo_objetos) != NULL){
-
-		struct objeto *objeto = objeto_crear_desde_string(linea);
-		if(!objeto){
-			fclose(archivo_objetos);
-			fclose(archivo_interacciones);
-			sala_destruir(sala);
-			return NULL;
-		}
-		lista_objetos = lista_insertar(lista_objetos, objeto);
-
-		if (!lista_objetos){
-			fclose(archivo_objetos);
-			fclose(archivo_interacciones);
-			sala_destruir(sala);
-			return NULL;
-		}
-	}
-
-	sala->objetos = lista_objetos;
 	
-	lista_t *lista_interacciones = malloc(sizeof(lista_t));
-
-	while( fgets( linea, MAX_LINEA, archivo_interacciones ) != NULL ) {
-
-		struct interaccion *interaccion = interaccion_crear_desde_string(linea);
-		if(!interaccion){
-			fclose(archivo_objetos);
-			fclose(archivo_interacciones);
-			sala_destruir(sala);
-			return NULL;
-		}
-		lista_interacciones = lista_insertar(lista_interacciones, interaccion);
-
-		if (!lista_interacciones){
-			fclose(archivo_objetos);
-			fclose(archivo_interacciones);
-			sala_destruir(sala);
-			return NULL;
-		}
-	}
-
-	sala->interacciones = lista_interacciones;
-
-	fclose(archivo_objetos);
-	fclose(archivo_interacciones);
-
-	if(sala->interacciones->cantidad == 0){
+	lista_t *lista_objetos = crear_lista_objetos(arch_objetos);
+	if(!lista_objetos){
+		fclose(arch_objetos);
+		fclose(arch_interacciones);
 		sala_destruir(sala);
 		return NULL;
 	}
+	sala->objetos = lista_objetos;
+	fclose(arch_objetos);
 
+	lista_t *lista_interaccion = crear_lista_interacciones(arch_interacciones);
+	if(!lista_interaccion){
+		fclose(arch_interacciones);
+		sala_destruir(sala);
+		return NULL;
+	}
+	sala->interacciones = lista_interaccion;
+	fclose(arch_interacciones);
+	
+	if(sala->interacciones->cantidad == 0 || sala->objetos->cantidad == 0){
+		return NULL;
+	}
+	
 	return sala;
 }
 
 char **sala_obtener_nombre_objetos(sala_t *sala, int *cantidad)
 {
-	if (sala == NULL) {
-		*cantidad = -1;
+	if(!sala){
+		*cantidad = ERROR;
 		return NULL;
 	}
 
-	if (cantidad == NULL) {
+	if(!cantidad){
 		int i = 0;
 		cantidad = &i;
 	}
 
 	*cantidad = (int)sala->objetos->cantidad;
-	char **nombres_objetos = malloc( (unsigned) (*cantidad) * ( sizeof(char *) ) );
+	char **nombres_objetos = malloc((unsigned)(*cantidad) * (sizeof(char *)));
 
-	if (nombres_objetos == NULL) {
-		free(nombres_objetos);
-		*cantidad = -1;
+	if(!nombres_objetos) {
+		*cantidad = ERROR;
 		return NULL;
 	}
 
-	for( int i = 0; i < *cantidad; i++ ) {
+	for(int i = 0; i < *cantidad; i++){
 		struct objeto *objeto = (struct objeto*)lista_elemento_en_posicion(sala->objetos, (size_t)i);
+		if(!objeto){
+			free(nombres_objetos);
+			return NULL;
+		}
 		nombres_objetos[i] = objeto->nombre;
 	}
 
