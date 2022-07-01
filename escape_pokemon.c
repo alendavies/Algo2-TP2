fb9a26c81f611214cb91294453dd069c71abe265
alendavieslec <alendavies@gmail.com>
2022-07-01 15:23:35 -0300

    anda

--- escape_pokemon.c
+++ escape_pokemon.c
@@ -4,6 +4,7 @@
 #include <string.h>
 
 #define ERROR -1
+#define MAX_STRING 100
 
 void imprimir_objetos_sala(sala_t *sala){
 
@@ -41,21 +42,194 @@ void imprimir_interacciones_sala(sala_t *sala){
 	sala_es_interaccion_valida(sala, "quemar", "mesa", "") ? printf("Válido\n") : printf("Inválido\n");
 }
 
+void mostrar_mensaje(const char *mensaje, enum tipo_accion accion, void *aux)
+{
+	printf("\n%s\n\n", mensaje);
+}
+
+void imprimir_objetos_conocidos(sala_t *sala)
+{
+	int cantidad_conocidos = 0;
+
+	char **conocidos = sala_obtener_nombre_objetos_conocidos(sala, &cantidad_conocidos);
+	if(!conocidos || cantidad_conocidos == ERROR){
+		printf("\nHubo un error al imprimir los objetos conocidos\n");
+	}
+	printf("\nObjetos conocidos:\n");
+	
+	for (int i = 0; i < cantidad_conocidos; i++) {
+		printf("%i: %s\n\n", i, conocidos[i]);
+	}
+
+	free(conocidos);
+}
+
+void imprimir_inventario(sala_t *sala)
+{
+	int cantidad_poseidos = 0;
+	char **poseidos = sala_obtener_nombre_objetos_poseidos(sala, &cantidad_poseidos);
+	if(!poseidos || cantidad_poseidos == ERROR){
+		printf("\nHubo un error al imprimir los objetos del inventario\n");
+	}
+	
+	printf("\nInventario:\n");
+	
+	for (int i = 0; i < cantidad_poseidos; i++) {
+		printf("%i: %s\n\n", i, poseidos[i]);
+	}
+	free(poseidos);
+}
+
+void imprimir_introduccion()
+{
+
+}
+
+int validar_opcion(sala_t *sala, char *comando, char *objeto1, char *objeto2, bool *ganado)
+{
+	if(!strcmp(comando, "ayuda")){
+
+		system("clear");
+
+		printf("\nEstos son los comandos disponibles:\n\n");
+		printf("\t-Para ver los objetos en el inventario ingresá (I).\n");
+		printf("\t-Para ver los objetos con los que podes interactuar ingresá (C).\n");
+		printf("\t-Para agarrar un objeto de la sala, ingresá 'agarrar' seguido del objeto.\n");
+		printf("\t-Para ver la descripcion de un objeto, ingresá 'descripcion' seguido del objeto.\n");
+		printf("\t-Para salir del juego ingresá 'salir'.\n");
+		printf("\n\tTambién podés realizar las siguientes acciones seguidas de los objetos conocidos de la sala: \n\n\t\t-Examinar.\n\t\t-Usar.\n\t\t-Abrir.\n\t\t-Salir.\n\n");
+	}
+
+	else if(!strcmp(comando, "agarrar")){
+		if(sala_agarrar_objeto(sala, objeto1)){
+			printf("\nNuevo objeto en el inventario: %s\n\n", objeto1);
+		}
+		else{
+			printf("\nNo se puede agarrar ese objeto\n\n");
+		}				
+	}
+
+	else if(!strcmp(comando, "describir")){
+		char *descripcion = sala_describir_objeto(sala, objeto1);
+		if(descripcion){
+			printf("%s\n", descripcion);
+		}	
+	}
+
+	else if(!strcmp(comando, "salir") && !strcmp(objeto1, "")){
+		system("clear");
+		return -1;
+	}
+
+	else if((!strcmp(comando, "I") || !strcmp(comando, "i")) && !strcmp(objeto1, "")){
+		imprimir_inventario(sala);
+	}
+
+	else if((!strcmp(comando, "C") || !strcmp(comando, "c")) && !strcmp(objeto1, "")){
+		imprimir_objetos_conocidos(sala);
+	}
+
+	else if(strcmp(comando, "") != 0 && strcmp(objeto1, "") != 0){
+		sala_ejecutar_interaccion(sala, comando, objeto1, objeto2, mostrar_mensaje, NULL);
+	}
+
+	if(sala_escape_exitoso(sala)){
+		printf("GANASTE\n");
+		*ganado = true;
+		return -1;
+	}
+
+	return 0;
+}
+
+void iniciar_juego(sala_t *sala)
+{
+	bool ganado = false;
+
+	imprimir_introduccion();
+
+	while(ganado == false){
+
+		char input[MAX_STRING];
+		char comando[MAX_STRING];
+		strcpy(comando, "");
+		char objeto1[MAX_STRING];
+		strcpy(objeto1, "");
+		char objeto2[MAX_STRING];
+		strcpy(objeto2, "");
+
+		fgets(input, MAX_STRING, stdin);
+
+		sscanf(input, "%s %s %s", comando, objeto1, objeto2);
+
+		int opcion = validar_opcion(sala, comando, objeto1, objeto2, &ganado);
+
+		if(opcion == -1){
+			return;
+		}
+	}
+}
+
+void mostrar_instrucciones()
+{
+}
+
+void menu_principal(sala_t *sala)
+{
+	char opcion;
+	printf("\nESCAPE POKEMON\n\n");
+	printf("> Iniciar el juego (J)\n\n");
+	printf("> Instrucciones del juego (I)\n\n");
+	printf("> Salir del juego (S)\n\n");
+
+	scanf(" %c", &opcion);
+
+	if(opcion == 'J' || opcion == 'j'){
+		system("clear");
+		iniciar_juego(sala);
+		menu_principal(sala);
+	}
+	else if(opcion == 'I' || opcion == 'i'){
+		system("clear");
+		mostrar_instrucciones();
+	}
+	else if(opcion == 'S' || opcion == 's'){
+
+		printf("\nEstás seguro que querés salir del juego? (y/n): ");
+		char opcion;
+		scanf(" %c", &opcion);
+
+		while(opcion != 'Y' && opcion != 'y' && opcion != 'N' && opcion != 'n'){
+			printf("Ingrese una opción válida (y/n): ");
+			scanf(" %c", &opcion);
+		}
+		if(opcion == 'Y' || opcion == 'y'){
+			return;
+		}
+		else if(opcion == 'N' || opcion == 'n'){
+			system("clear");
+			menu_principal(sala);
+		}	
+	}
+}
+
 int main(int argc, char *argv[])
 {
-	if(argc != 3){
+	if (argc != 3){
 		return ERROR;
 	}
 	sala_t *sala = sala_crear_desde_archivos(argv[1], argv[2]);
 
-	if (sala == NULL) {
-		printf("Error al crear la sala de escape\n");
+	if (!sala) {
+		printf("\nError al crear la sala de escape\n");
 		return ERROR;
 	}
 
-	imprimir_objetos_sala(sala);
+	menu_principal(sala);
 
-	imprimir_interacciones_sala(sala);
+	//imprimir_objetos_sala(sala);
+	//imprimir_interacciones_sala(sala);
+
 
 	sala_destruir(sala);
 
--- src/sala.c
+++ src/sala.c
@@ -2,20 +2,28 @@
 #include "sala.h"
 #include "objeto.h"
 #include "interaccion.h"
+#include "lista.h"
 #include <stdlib.h>
 #include <stdio.h>
 #include <string.h>
-#include "lista.h"
-
-struct sala {
-	lista_t *objetos;
-	lista_t *interacciones;
-	bool escape_exitoso;
-};
 
 #define MAX_CARACTERES 1024
 #define CANT_OBJETOS 20
 #define ERROR -1
+
+struct sala {
+	lista_t *objetos;
+	lista_t *interacciones;
+	lista_t *conocidos;
+	lista_t *poseidos;
+	bool escape_exitoso;
+};
+
+int comparador(void *elemento, void *contexto)
+{
+	struct objeto *objeto = (struct objeto *)elemento;
+	return strcmp(objeto->nombre, (const char *)contexto);
+}
 
 lista_t *crear_lista_objetos(FILE *archivo)
 {
@@ -67,6 +75,26 @@ lista_t *crear_lista_interacciones(FILE *archivo)
 	return lista_interacciones;
 }
 
+bool conocer_objeto(sala_t *sala, const char *nombre_objeto)
+{
+	if(!sala || !nombre_objeto){
+		return false;
+	}
+
+	if(lista_buscar_elemento(sala->conocidos, comparador, (void *)nombre_objeto) != NULL){
+		return false;
+	}
+
+	struct objeto *objeto = lista_buscar_elemento(sala->objetos, comparador, (void *)nombre_objeto);
+
+	if(objeto){
+		lista_insertar(sala->conocidos, objeto);
+		return true;
+	}
+
+	return false;
+}
+
 sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones)
 {	
 	if(!objetos || !interacciones){
@@ -89,7 +117,7 @@ sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones
 		fclose(arch_objetos);
 		fclose(arch_interacciones);
 		return NULL;
-	}
+	}	
 	
 	lista_t *lista_objetos = crear_lista_objetos(arch_objetos);
 	if(!lista_objetos){
@@ -114,6 +142,23 @@ sala_t *sala_crear_desde_archivos(const char *objetos, const char *interacciones
 		sala_destruir(sala);
 		return NULL;
 	}
+
+	lista_t *lista_conocidos = lista_crear();
+	if(!lista_conocidos){
+		sala_destruir(sala);
+		return NULL;
+	}
+
+	lista_t *lista_poseidos = lista_crear();
+	if(!lista_poseidos){
+		sala_destruir(sala);
+		return NULL;
+	}
+
+	sala->conocidos = lista_conocidos;
+	sala->poseidos = lista_poseidos;
+
+	conocer_objeto(sala, "habitacion");
 	
 	return sala;
 }
@@ -150,6 +195,64 @@ char **sala_obtener_nombre_objetos(sala_t *sala, int *cantidad)
 	return nombres_objetos;
 }
 
+char **sala_obtener_nombre_objetos_conocidos(sala_t *sala, int *cantidad)
+{
+	if (!sala) {
+		*cantidad = ERROR;
+		return NULL;
+	}
+
+	if (!cantidad) {
+		int i = 0;
+		cantidad = &i;
+	}
+
+	*cantidad = (int)sala->conocidos->cantidad;
+
+	char **conocidos = malloc((unsigned)(*cantidad) * (sizeof(char *)));
+
+	if (!conocidos){
+		*cantidad = ERROR;
+		return NULL;
+	}
+
+	for(int i = 0; i < *cantidad; i++){
+		
+		struct objeto *objeto = (struct objeto*)lista_elemento_en_posicion(sala->conocidos, (size_t)i);
+		conocidos[i] = objeto->nombre;
+	}
+
+	return conocidos;
+}
+
+char **sala_obtener_nombre_objetos_poseidos(sala_t *sala, int *cantidad)
+{
+	if (!sala) {
+		*cantidad = ERROR;
+		return NULL;
+	}
+
+	if (!cantidad) {
+		int i = 0;
+		cantidad = &i;
+	}
+
+	*cantidad = (int)sala->poseidos->cantidad;
+	char **poseidos = malloc((unsigned)(*cantidad) * (sizeof(char *)));
+
+	if(!poseidos){
+		*cantidad = ERROR;
+		return NULL;
+	}
+
+	for(int i = 0; i < *cantidad; i++){
+		struct objeto *objeto = (struct objeto*)lista_elemento_en_posicion(sala->poseidos, (size_t)i);
+		poseidos[i] = objeto->nombre;
+	}
+
+	return poseidos;
+}
+
 bool sala_es_interaccion_valida(sala_t *sala, const char *verbo, const char *objeto1, const char *objeto2)
 {
 	if(!sala || !verbo || !objeto1 || !objeto2){
@@ -164,9 +267,9 @@ bool sala_es_interaccion_valida(sala_t *sala, const char *verbo, const char *obj
 		strcmp(interaccion->objeto, objeto1) == 0 &&
 		strcmp(interaccion->objeto_parametro, objeto2) == 0){
 		 	return true;
-		}
-			
+		}	
 	}
+
 	return false;
 }
 
@@ -175,15 +278,16 @@ bool sala_agarrar_objeto(sala_t *sala, const char *nombre_objeto)
 	if(!sala || !nombre_objeto){
 		return false;
 	}
-	for(int i = 0; i < sala->objetos->cantidad; i++){
-		struct objeto *objeto = lista_elemento_en_posicion(sala->objetos, (size_t)i);
-		if(!objeto){
-			return false;
-		}
-		if(strcmp(objeto->nombre, nombre_objeto) == 0){
-			return objeto->es_asible;
-		}
+
+	struct objeto *objeto_a_agarrar = lista_buscar_elemento(sala->poseidos, comparador, (void *)nombre_objeto);
+
+	struct objeto *objeto_conocido = lista_buscar_elemento(sala->conocidos, comparador, (void *)nombre_objeto);
+	
+	if(!objeto_a_agarrar && objeto_conocido != NULL && objeto_conocido->es_asible){
+		lista_insertar(sala->poseidos, objeto_conocido);
+		return true;
 	}
+
 	return false;
 }
 
@@ -192,76 +296,140 @@ char* sala_describir_objeto(sala_t* sala, const char *nombre_objeto)
 	if(!sala || !nombre_objeto){
 		return NULL;
 	}
-	for(int i = 0; i < sala->objetos->cantidad; i++){
-		struct objeto *objeto = lista_elemento_en_posicion(sala->objetos, (size_t)i);
+
+	struct objeto *objeto_poseido = lista_buscar_elemento(sala->poseidos, comparador, (void *)nombre_objeto);
+
+	struct objeto *objeto_conocido = lista_buscar_elemento(sala->conocidos, comparador, (void *)nombre_objeto);
+	
+	if(objeto_poseido || objeto_conocido){
+		return objeto_poseido->descripcion;
+	}
+
+	return NULL;
+}
+
+lista_t *lista_quitar_elemento(lista_t *lista, int (*comparador)(void *, void *), void *contexto)
+{
+	if (!lista || !comparador || lista_vacia(lista)){
+		return NULL;
+	}
+
+	for (size_t i = 0; i < lista_tamanio(lista); i++) {
+		struct objeto *objeto = lista_elemento_en_posicion(lista, i);
 		if(!objeto){
 			return NULL;
 		}
-		if(strcmp(objeto->nombre, nombre_objeto) == 0){
-			return objeto->descripcion;
+		if(comparador(objeto, contexto) == 0){
+			objeto = lista_quitar_de_posicion(lista, i);
+			if(!objeto){
+				return NULL;
+			}
 		}
 	}
-	return NULL;
+
+	return lista;
+}
+
+bool eliminar_objeto(sala_t *sala, const char *nombre_objeto)
+{
+	if(!sala || !nombre_objeto){
+		return false;
+	}
+
+	lista_t *poseidos = lista_quitar_elemento(sala->poseidos, comparador, (void *)nombre_objeto);
+	lista_t *conocidos = lista_quitar_elemento(sala->conocidos, comparador, (void *)nombre_objeto);
+	lista_t *objetos = lista_quitar_elemento(sala->objetos, comparador, (void *)nombre_objeto);
+
+	if(!poseidos || !conocidos || !objetos){
+		return false;
+	}
+
+	sala->poseidos = poseidos;
+	sala->conocidos = conocidos;
+	sala->objetos = objetos;
+
+	return true;
+}
+
+bool interaccion_correcta(struct interaccion *interaccion, const char *verbo, const char *objeto1, const char *objeto2)
+{
+	if (strcmp(interaccion->verbo, verbo) == 0 && 
+	strcmp(interaccion->objeto, objeto1) == 0 && 
+	strcmp(interaccion->objeto_parametro, objeto2) == 0){
+		return true;
+	}
+
+	return false;
 }
 
 int ejecutar_accion(sala_t *sala, struct interaccion *interaccion, const char *objeto1, const char *objeto2, void (*mostrar_mensaje)(const char *mensaje, enum tipo_accion accion, void *aux), void *aux){
 
 	int ejecutadas = 0;
+
+	if(interaccion->accion.tipo == ACCION_INVALIDA){
+		return ejecutadas;
+	}
 	
-	if(interaccion->accion.tipo == MOSTRAR_MENSAJE){
-		mostrar_mensaje(interaccion->accion.mensaje, interaccion->accion.tipo, NULL);
+	else if(interaccion->accion.tipo == MOSTRAR_MENSAJE){
 		ejecutadas++;
 	}
-	if(interaccion->accion.tipo == ELIMINAR_OBJETO){
-		for(int i = 0; i < sala->objetos->cantidad; i++){
-			struct objeto *objeto = lista_elemento_en_posicion(sala->objetos, (size_t)i);
-			if(strcmp(objeto->nombre, objeto1) == 0){
-				lista_quitar_de_posicion(sala->objetos, (size_t)i);
-			}
+
+	else if(interaccion->accion.tipo == ELIMINAR_OBJETO){
+		if(eliminar_objeto(sala, objeto1)){
+			ejecutadas++;
 		}
-		ejecutadas++;
 	}
-	if(interaccion->accion.tipo == DESCUBRIR_OBJETO){
-		//Conocer objeto
-		ejecutadas++;
+
+	else if(interaccion->accion.tipo == DESCUBRIR_OBJETO){
+		if(conocer_objeto(sala, interaccion->accion.objeto)){
+			ejecutadas++;
+		}
 	}
-	if(interaccion->accion.tipo == ESCAPAR){
+
+	else if(interaccion->accion.tipo == ESCAPAR){
 		sala->escape_exitoso = true;
 		ejecutadas++;
 	}
-	if(interaccion->accion.tipo == REEMPLAZAR_OBJETO){
-		for(int i = 0; i < sala->objetos->cantidad; i++){
-			struct objeto *objeto_parametro = lista_elemento_en_posicion(sala->objetos, (size_t)i);
-			if(strcmp(objeto_parametro->nombre, objeto2) == 0){
-				lista_quitar_de_posicion(sala->objetos, (size_t)i);
-			}
+	
+	else if(interaccion->accion.tipo == REEMPLAZAR_OBJETO){
+		if(eliminar_objeto(sala, objeto2) && conocer_objeto(sala, interaccion->accion.objeto)){
+			ejecutadas++;
 		}
-		//descubrir nuevo objeto
-		ejecutadas++;
 	}
+
+	mostrar_mensaje(interaccion->accion.mensaje, interaccion->accion.tipo, aux);
+
 	return ejecutadas;
 }
 
 int sala_ejecutar_interaccion(sala_t *sala, const char *verbo, const char *objeto1, const char *objeto2, void (*mostrar_mensaje)(const char *mensaje, enum tipo_accion accion, void *aux), void *aux)
 {
-	if(!sala || !verbo || !objeto1 || !objeto2 || !mostrar_mensaje || !aux){
+	if(!sala || !verbo || !objeto1 || !objeto2 || !mostrar_mensaje){
 		return 0;
 	}
+
+	if(!lista_buscar_elemento(sala->conocidos, comparador, (void *)objeto1) && !lista_buscar_elemento(sala->poseidos, comparador, (void *)objeto1)){
+
+		return 0;
+	}
+
 	if(!sala_es_interaccion_valida(sala, verbo, objeto1, objeto2)){
+
 		return 0;
 	}
+	
 	int ejecutadas = 0;
+
 	for(int i = 0; i < sala->interacciones->cantidad; i++){
 
-		struct interaccion *interaccion = (struct interaccion *)lista_elemento_en_posicion(sala->interacciones, (size_t)i);
+		struct interaccion *interaccion = lista_elemento_en_posicion(sala->interacciones, (size_t)i);
 
-		if(strcmp(interaccion->verbo, verbo) == 0 &&
-		strcmp(interaccion->objeto, objeto1) == 0 &&
-		strcmp(interaccion->objeto_parametro, objeto2) == 0){
+		if(interaccion_correcta(interaccion, verbo, objeto1, objeto2)){
 			
 			ejecutadas = ejecutar_accion(sala, interaccion, objeto1, objeto2, mostrar_mensaje, aux);
 		}			
 	}
+
 	return ejecutadas;
 }
 
@@ -278,7 +446,10 @@ void sala_destruir(sala_t *sala)
 	if(!sala){
 		return;
 	}
+
 	lista_destruir_todo(sala->objetos, free);
 	lista_destruir_todo(sala->interacciones, free);
+	lista_destruir(sala->conocidos);
+	lista_destruir(sala->poseidos);
 	free(sala);
 }
